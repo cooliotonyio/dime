75466c10b6f7b7e762df4c07fb54e1f1ee7a7277
trinhdrew1418 <trinhdroo1418@gmail.com>
2019-04-19 22:10:30 -0700

    converted NUSWIDE to use inheritance

--- datasets.py
+++ datasets.py
@@ -9,9 +9,14 @@ import torch
 import pickle
 
 class BaseCMRetrievalDataset(Dataset):
-    def __init__(self, root, transform, target_transform=None primary_tags=None, secondary_tags=None, feature_mode='vanilla'):
-        super(BaseCMRetrievalDataset, self)
-        self._foldernames, self._folder_to_idx = self._find_folders(root)
+    """
+    Description:
+        Base dataset to build off of for other datasets for crossmodal retrieval tasks
+
+    """
+    def __init__(self, root, transform, primary_tags=None, secondary_tags=None):
+        super(BaseCMRetrievalDataset, self).__init__()
+        self._folder_names, self._folder_to_idx = self._find_folders(root)
 
         self.image_paths, self._folder_targets = self._make_base(self, root, self._folder_to_idx)
 
@@ -23,6 +28,7 @@ class BaseCMRetrievalDataset(Dataset):
             self.primary_tags = primary_tags
 
         self.secondary_tags = secondary_tags
+
 
     def _find_folders(self, dir):
         """
@@ -41,7 +47,20 @@ class BaseCMRetrievalDataset(Dataset):
         folder_to_idx = {folder_names[i] : i for i in range(len(folder_names))}
         return folder_names, folder_to_idx
 
+
     def _make_base(self, dir, folder_to_idx):
+        """
+        Produces the two most basic elements of the dataset, inputs (images) and targets
+
+        Args:
+            dir (string): Directory path
+
+        Returns:
+            tuple: (images, folder_targets).
+                - images (list): images[i] = image path of i'th image
+                - folder_targets (list): folder_targets[i] = the index of the folder that the i'th image came from
+        """
+
         POS_EXT = ('.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm', '.tif', '.tiff', '.webp')
         images = []
         folder_targets = []
@@ -61,6 +80,7 @@ class BaseCMRetrievalDataset(Dataset):
 
         return images, folder_targets
 
+
     def __getitem__(self, index):
         path = self.images[index]
         with open(path, 'rb') as f:
@@ -73,27 +93,75 @@ class BaseCMRetrievalDataset(Dataset):
 
         return img, target
 
+
+    def get_primary_tags(self, index):
+        """
+        Args:
+            index of image
+
+        Returns:
+            primary_tags (list): list of strings that are related to the image of the given index
+        """
+
+        return self.primary_tags[index]
+
+
+    def get_secondary_tags(self, index):
+        """
+        Args:
+            index of image
+
+        Returns:
+            secondary_tags (list): list of strings that are related to the image of the given index
+        """
+
+        return self.secondary_tags[index]
+
+
     def __len__(self):
         return len(self.images)
 
 
-class NUS_WIDE(Dataset):
-    def __init__(self, root, transform, features='resnet152'):
-        self.imgs = tv.datasets.ImageFolder(root=root)
-        self.transform = transform
+class NUS_WIDE(BaseCMRetrievalDataset):
+    """
+    Description:
+        Dataset class to manage the NUSWIDE dataset
 
-        if features == 'resnet152':
-            self.features, self.feature_mode = pickle.load(open("pickles/nuswide_features/resnet152_nuswide_feats_arr.p","rb")), 'resnet152'
-        elif features == 'resnet18':
-            self.features, self.feature_mode = pickle.load(open("pickles/nuswide_features/resnet18_nuswide_feats_arr.p", "rb")), 'resnet18'
+    """
+
+    def __init__(self, root, transform, feature_mode='resnet152', word_embeddings='fastText'):
+        primary_tags = pickle.load(open("pickles/nuswide_metadata/tag_matrix.p", "rb"))
+
+        super(NUS_WIDE, self).__init__(root, transform, primary_tags=primary_tags)
+
+        self.primary_tags = self._filter_unavailable(self.primary_tags, )
+        self.secondary_tags = self._make_secondary_tags()
+
+        self.feature_mode = feature_mode
+
+        if feature_mode == 'resnet152':
+            self.features = pickle.load(open("pickles/nuswide_features/resnet152_nuswide_feats_arr.p","rb"))
+        elif feature_mode == 'resnet18':
+            self.features = pickle.load(open("pickles/nuswide_features/resnet18_nuswide_feats_arr.p", "rb"))
         else:
             self.features, self.feature_mode = None, 'vanilla'
+
+        if word_embeddings == 'fastText':
+            self.word_embeddings = pickle.load(open("pickles/word_embeddings/word_embeddings_tensors.p", "rb"))
 
         self.positive_concept_matrix = pickle.load(open("pickles/nuswide_metadata/concept_matrix.p", "rb"))
         self.negative_concept_matrix = pickle.load(open("pickles/nuswide_metadata/neg_concept_matrix.p", "rb"))
         self.relevancy_matrix = pickle.load(open("pickles/nuswide_metadata/relevancy_matrix.p", "rb"))
-        self.tag_matrix = pickle.load(open("pickles/nuswide_metadata/tag_matrix.p", "rb"))
-        self.folder_labels = pickle.load(open("pickles/nuswide_metadata/folder_labels.p", "rb"))
+
+
+    def _make_secondary_tags(self):
+        """
+        The secondary tags for NUS-WIDE are the associated folder names that the images reside in
+
+        """
+
+        return [self._folder_names[self._folder_targets[i]] for i in range(len(self))]
+
 
     def __getitem__(self, index):
         """
@@ -102,13 +170,17 @@ class NUS_WIDE(Dataset):
         Returns:
             tuple: (index, data, target) where target is class_index of the target class.
         """
+
+        sample, target = super().__getitem__(index)
+
         if self.feature_mode is not 'vanilla':
-            return index, self.features[index], self.imgs[index][1]
+            return index, self.features[index], target
 
         if self.transform is not None:
-            return index, self.transform(self.imgs[index][0]), self.imgs[index][1]
+            return index, self.transform(sample), target
 
-        return index, self.data[index], self.labels[index]
+        return index, sample, target
+
 
     def get_concepts(self, index):
         """
@@ -117,7 +189,41 @@ class NUS_WIDE(Dataset):
         Returns:
             List of concepts (strings) for the image
         """
+
         return self.positive_concept_matrix[index]
+
+
+    def get_random_primary_tag(self, index, dtype='embedding'):
+        """
+        Obtains a random associated primary tag for the given image index in
+        either the format of the word embedding or a string
+
+        Args:
+            index: index of the desired image
+            dtype: either 'embedding' or 'string'
+
+        Returns:
+            random primary tag
+        """
+
+        return self._get_random_embedding(index, mode='primary', dtype=dtype)
+
+
+    def get_random_secondary_tag(self, index, dtype='embedding'):
+        """
+        Obtains a random associated secondary tag for the given image index in
+        either the format of the word embedding or a string
+
+        Args:
+            index: index of the desired image
+            dtype: either 'embedding' or 'string'
+
+        Returns:
+            random secondary tag
+        """
+
+        return self._get_random_embedding(index, mode='secondary', dtype=dtype)
+
 
     def get_negative_concepts(self, index):
         """
@@ -126,19 +232,43 @@ class NUS_WIDE(Dataset):
         Returns:
             List of negative concepts (strings) for the image
         """
+
         return self.negative_concept_matrix[index]
 
-    def get_folder_label(self, index):
-        """
-        Args:
-            index (int): Index of image
-        Returns:
-            label of folder (string)
-        """
-        return self.folder_labels[index]
 
-    def __len__(self):
-        return len(self.imgs)
+    def _filter_unavailable(tags, embedding_dictionary):
+        """
+        Filters out the unavailable tags given the available embeddings
+
+        Args:
+            tags (list of lists): tags[i] = list of associated tags for i'th image
+            embedding_dictionary (dictionary): (word:word embedding tensor)
+
+        Returns:
+            tags (list of lists): same as the argument except modified to remove the tags
+                                    that are unavailable
+        """
+
+        for tag_list in tags:
+            for tag in tag_list:
+                if tag not in embedding_dictionary:
+                    tag_list.remove(tag)
+
+        return tags
+
+
+    def _get_random_embedding(self, index, mode, dtype):
+        if mode == 'primary':
+            tag_set = self.primary_tags
+        else:
+            tag_set = self.secondary_tags
+
+        tag_list = tag_set[index]
+        random_tag = random.choice(tag_list)
+        if mode == 'embedding':
+            return self.word_embeddings[random_tag]
+
+        return random_tag
 
 
 # Dataset used for nearest neighbors loading
@@ -156,6 +286,7 @@ class NUS_WIDE_KNN(Dataset):
         Returns:
             tuple: (sample, target) where target is class_index of the target class.
         """
+
         if self.features is not None:
             return self.features[index], index
         return self.transform(self.imgs[index][0]), index
