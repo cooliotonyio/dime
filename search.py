1108fd59e7eedccd8194d8787b5a28a85089b1c2
cooliotonyio <tonyzhao@berkeley.edu>
2019-05-23 21:30:11 +0000

    wooo

--- search.py
+++ search.py
@@ -7,36 +7,69 @@ import time
 from torchvision import transforms
 from sklearn.preprocessing import binarize
 
-class EmbeddingModel():
+class Model():
     '''
     Wrapper Class around a neural network
     '''
-    def __init__(self, net, name, modality, input_dimension, output_dimension, cuda):
+    def __init__(self, net, name, modalities, embedding_functions, input_dimensions, output_dimension, cuda, desc):
+        '''
+        Initializes Model obkect
+        
+        Parameters:
+        net (neural network): The neural network to be used
+        name (string): Name of network, used as dictionary key
+        modalities (list of strings): Ordered list of modalities
+        embedding_functions (list of methods): Ordered list of forward function
+        input_dimensions (list of ints): Ordered list of input dimensions
+        output dimension (int): Output dimension of the model
+        cuda (bool): True if CUDA is being used
+        desc (string): Description of model
+        
+        '''
         self.net = net
         self.name = name
-        self.modality = modality
-        self.input_dimension = input_dimension
+        self.modalities = modalities
+        self.embedding_functions = embedding_functions
+        self.input_dimensions = input_dimensions
         self.output_dimension = output_dimension
         self.cuda = cuda
+        if desc is None:
+            desc = name
+        self.desc = desc
         if self.cuda:
             self.net.cuda()
         else:
             self.net.cpu()
     
-    def get_embedding(self, tensor):
-        return self.net.get_embedding(tensor)
+    def get_embedding(self, tensor, modality):
+        for i in range(len(self.modalities)):
+            if modality == self.modalities[i]:
+                return self.embedding_functions[i](tensor)
+        raise RuntimeError("Modality '{}' not supported by this model".format(modality))
     
     def to_cuda(self):
         return self.net.cuda()
     
     def to_cpu(self):
-        return self.cpu.cuda()
+        return self.net.cpu()
+    
+    def get_info(self):
+        info = {
+            "name": self.name,
+            "modalities": self.modalities,
+            "input_dimensions": self.input_dimensions,
+            "output_dimension": self.output_dimension,
+            "cuda": self.cuda,
+            "desc": self.desc
+        }
+        return info
+    
 
 class Dataset():
     '''
     Wrapper class around a dataset
     '''
-    def __init__(self, data_loader,data, name, modality, dimension):
+    def __init__(self, data_loader, data, name, modality, dimension):
         self.data = data
         self.data_loader = data_loader
         self.name = name
@@ -45,17 +78,8 @@ class Dataset():
 
     def create_loader(self, model, binarized, threshold, save_directory, load_embeddings, batch_size, cuda):
         '''
-        Just sanity checks before fitting data. Moved this here for cleaer code
-        
-        Called by SearchEngine.fit()
-        
-        Parameters:
-        data (iterable): Some iterable that has interations of (batch index, batch embeddings)
-        save_embeddings (bool): If True, write each batch into a file (overwrites)
-        load_embeddings (bool): If True, load embeddings from self.save_directory
-        
         Returns:
-        iterable: Loader that yields baches
+        iterable: Loader that yields batches
         '''
         
         #TODO: enable loading saved_embeddings midway
@@ -72,11 +96,11 @@ class Dataset():
         Saves batch into a filename into .npy file
         Does bitpacking if batches are binarized to drastically reduce size of files
         
-        Called by SearchEngine.fit(save_embeddings = True)
-        
         Parameters:
         batch (arraylike): Batch to save
         filename (string): Path to save batch to
+        binarized (bool): Binarized or not
+        save_directory (string): Directory to save .npy files
         
         Returns:
         None
@@ -96,6 +120,8 @@ class Dataset():
         
         Parameters:
         filename (string): Path to batch, which should be a .npy file
+        model (EmbeddingModel): Model that created the batches, need to correctly format binarized arrays
+        binarized (bool): Whether arrays are binarized or not
         
         Returns:
         arraylike: loaded batch
@@ -113,9 +139,9 @@ class Dataset():
         Loads previously saved embeddings from save_directory
         
         Parameters:
-        directory: Directory of embeddings
-        model: Model object that outputted the saved embeddings
-        binarized: Boolean, True if the saved embedding is binarized. False otherwise.
+        directory (string): Directory of embeddings
+        model (EmbeddingModel): Model object that outputted the saved embeddings
+        binarized (bool): True if the saved embedding is binarized. False otherwise.
         
         Yields:
         int: Batch index
@@ -126,14 +152,16 @@ class Dataset():
             embeddings = self.load_batch(filenames[batch_idx], model, binarized)
             yield batch_idx, embeddings
     
-    def featurize_data(self, model, binarized, offset, cuda):
+    def featurize_data(self, model, binarized, offset, cuda, threshold = 0):
         '''
-        Generator function that yields embeddings of data in batches
-        
-        Called by fit(load_embeddings = False)
+        Generator function that takes in a model and returns the features of dataset
         
         Parameters:
-        data (arraylike): Data to featurize
+        model (Model): Model used to extract features
+        binarized (bool): Whether resulting feature vectors should be binarized
+        offset (int): Which data index to start yielding feature vectors from
+        cuda (bool): Whether CUDA is being used
+        threshold (float): Threshold for binarization, used only for binarization
         
         Yields:
         int: Batch index
@@ -145,46 +173,22 @@ class Dataset():
                     data = (data,)
                 if cuda:
                     data = tuple(d.cuda() for d in data)
-                embeddings = model.get_embedding(*data)
+                embeddings = model.get_embedding(*data, self.modality)
                 if binarized:
-                    embeddings = binarize(embeddings.detach(), threshold=self.threshold)
+                    embeddings = binarize(embeddings.detach(), threshold=threshold)
                 yield batch_idx, embeddings.cpu().detach().numpy()
 
 class SearchEngine():
     '''
     Search Engine Class
-    
-    Attributes: 
-        embedding_net (neural network): Neural network to pass in tensors and receive embeddings
-        cuda (bool): If True, enables CUDA for featurizing data
-        is_binarized (bool): If True, all embeddings are binarized
-        threshold (float): Threshold level for binarization
-        save_directory (string): Directory to save/load embeddings
-        embeddings_name (string): Name for filenames of embeddings
-        index (faiss index): Index of featurized data
-    
-    User Methods:
-        fit()
-        text_to_tensor()
-        image_to_tensor()
-        get_embedding()
-        search()
-        
     '''
 
     def __init__(self, modalities, cuda = False, save_directory = None, verbose = False):
         '''
         Initializes SearchEngine object
         
-        Called by User
-        
         Parameters:
-        embedding_net (neural network): Neural network to pass in tensors and receive embeddings
-        cuda (bool): If True, enables CUDA for featurizing data
-        is_binarized (bool): If True, all embeddings are binarized
-        threshold (float): Threshold level for binarization
-        save_directory (string): Directory to save/load embeddings
-        embeddings_name (string): Name for filenames of embeddings
+        modalities (list of strings): Which modalities this SearchEngine object supports
         
         Returns:
         SearchEngine: SearchEngine object
@@ -210,8 +214,8 @@ class SearchEngine():
     def valid_models(self, tensor, modality):
         valid_models = []
         for model in list(self.models.values()):
-            if model.modality == modality:
-                if model.input_dimension == tuple(tensor.shape):
+            for i in range(len(model.modalities)):
+                if modality == model.modalities[i] and model.input_dimensions[i] == tuple(tensor.shape):
                     valid_models.append(model.name)
         return valid_models
     
@@ -223,10 +227,10 @@ class SearchEngine():
                 valid_indexes_keys.append(key)
         return valid_indexes_keys
         
-    def get_embedding(self, tensor, model_name, binarized = False, threshold = 0):
+    def get_embedding(self, tensor, model_name, modality, binarized = False, threshold = 0):
         assert model_name in self.models, "Model not found"
         model = self.models[model_name]
-        embedding = model.get_embedding(tensor)
+        embedding = model.get_embedding(tensor, modality)
         if binarized:
             embedding = binarize(embedding)
         return embedding
@@ -234,17 +238,6 @@ class SearchEngine():
     def search(self, embeddings, index_key, n=5):
         '''
         Searches index for nearest n neighbors for each embedding in embeddings
-        
-        Called by User
-        
-        Parameters:
-        embeddings (np.array): Vector or list of vectors to search
-        index_key (tuple): Key of the index to search through
-        n (int): Number of neighbors to return for each embedding
-        
-        Returns
-        list: List of lists of distances of neighbors
-        list: List of lists of indexes of neighbors
         '''
         assert index_key in self.indexes, "Index key not recognized"
         index = self.indexes[index_key]
@@ -258,21 +251,20 @@ class SearchEngine():
             return distances[0], idxs[0]
         else:
             return distances, idxs
+        
+    def get_model_info(self, model_name):
+        return self.models[model_name].get_info()
     
-    def add_model(self, net, name, modality, input_dimension, output_dimension):
+    def add_model(self, net, name, modalities, embedding_functions, input_dimensions, output_dimension, desc=None):
 
         assert (name not in self.models), "Model with given name already in self.models"
-        assert (modality in self.modalities), "Modality not supported by SearchEngine"
-
-        model = EmbeddingModel(net, name, modality, input_dimension, output_dimension, self.cuda)
+        for modality in modalities:
+            assert (modality in self.modalities), "Modality not supported by SearchEngine"
+        
+        model = Model(net, name, modalities, embedding_functions, input_dimensions, output_dimension, self.cuda, desc)
         self.models[name] = model
-        self.modalities[modality]['models'].append(name)
-
-    def print_models(self):
-        print("{} \t {} \t {}".format("Index", "Model Name", "Modality"))
-        for i, key in enumerate(self.models):
-            model = self.models[key]
-            print("{} \t {} \t {}".format(i, model.name, model.modality))
+        for modality in modalities:
+            self.modalities[modality]['models'].append(name)
 
     def add_dataset(self, dataset_name, data_loader, data, modality, dimension):
         assert (dataset_name not in self.datasets), "Dataset with dataset_name already in self.datasets"
@@ -298,9 +290,11 @@ class SearchEngine():
         
         models = [self.models[model] for model in models]
         
-        for model in models:
-            assert dataset.modality == model.modality, "Model modality does not match dataset modality"
-            assert model.input_dimension == dataset.dimension, "Model input dimension does not match dimension of dataset"
+        for model in models:          
+            assert dataset.modality in model.modalities, "Model does not support dataset modality"
+            for i in range(len(model.modalities)):
+                if dataset.modality == model.modalities[i]:
+                    assert model.input_dimensions[i] == dataset.dimension, "Model input dimension incompatible"
             index = faiss.IndexFlatL2(model.output_dimension)
             key = (dataset_name, model.name, binarized)
 
@@ -310,31 +304,16 @@ class SearchEngine():
 
             #TODO: Cuda() index
 
-            self.fit(index, model, dataset,binarized, threshold, load_embeddings, save_embeddings, batch_size, step_size)
+            self.fit(
+                index, model, dataset, binarized, threshold, load_embeddings, save_embeddings, batch_size, step_size)
 
-    def fit(self, index, model, dataset,binarized, threshold, load_embeddings, save_embeddings, batch_size, step_size):
-        '''
-        1) Featurize (and optionally binarize) data into embeddings OR load embeddings without data
-        2) Optionally save embeddings
-        3) Add embeddings into index
-        
-        Called by build_index
-            
-        Parameters:
-        data (iterable): Some iterable that has interations of (batch index, batch embeddings)
-        save_embeddings (bool): If True, write each batch into a file (overwrites)
-        load_embeddings (bool): If True, load embeddings from self.save_directory
-        verbose (bool): If True, prints metadata every step_size
-        step_size (int): Number of batches to process before printing metadata
-        
-        Returns:
-        None
-        '''
+    def fit(self, index, model, dataset, binarized, threshold, load_embeddings, save_embeddings, batch_size, step_size):
         if self.verbose:
             start_time = time.time()
-            print("Building {} index".format(model.name))
+            print("Building {},{} index".format(model.name, dataset.name))
             
-        data_loader = dataset.create_loader(model, binarized, threshold, self.save_directory, load_embeddings, batch_size, self.cuda)
+        data_loader = dataset.create_loader(
+            model, binarized, threshold, self.save_directory, load_embeddings, batch_size, self.cuda)
         
         #TODO: fix num_batches
         num_batches = 10000
@@ -361,6 +340,5 @@ class SearchEngine():
         return [dataset.data[i] for i in indicies]
      
     def __repr__(self):
-        '''
-        '''
-        return "SearchEngine<{} modalities, {} models, {} datasets, {} indexes>".format(len(self.modalities), len(self.models), len(self.datasets), len(self.indexes))
+        return "SearchEngine<{} modalities, {} models, {} datasets, {} indexes>".format(
+            len(self.modalities), len(self.models), len(self.datasets), len(self.indexes))
