ff5ae2d5b30510feff3ad5674ea2d70b0e84ceab
cooliotonyio <tonyzhao@berkeley.edu>
2019-05-24 14:44:03 +0000

    we're getting there

--- server.py
+++ server.py
@@ -1,10 +1,5 @@
 from flask import Flask, request, redirect, url_for, send_from_directory, jsonify
-
 from werkzeug.utils import secure_filename
-from torchvision import transforms
-from torchvision.datasets import ImageFolder
-from torch.utils.data import DataLoader
-
 import PIL
 import pickle
 import os
@@ -12,14 +7,22 @@ import traceback
 import json
 import logging
 
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+
 from search import SearchEngine
 from networks import FeatureExtractor
 
 EMBEDDING_DIR = ""
-UPLOAD_DIR = "./uploads"
-DATA_DIR = "./data"
+UPLOAD_DIR = "uploads/"
+DATA_DIR = "data/"
+IMAGE_DIR = 'data/Flickr'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 FAST_TEXT = None
+EMBED_DIR = 'embeddings/'
+BATCH_SIZE = 128
+LOAD_EMBED = True
 
 app = Flask(__name__)
 logging.getLogger('werkzeug').setLevel(logging.ERROR)
@@ -28,52 +31,74 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 def init_engine(app):
+    # Instantiate SearchEngine Object
+    search_engine = SearchEngine(["text", "image"], save_directory = EMBED_DIR, verbose = True)
 
-    #Building SearchEngine
-    print("Building search engine")
-    save_directory = './embeddings'
-    search_engine = SearchEngine(["text", "image"], save_directory = save_directory, verbose = True)
-
-    # Build Networks
-    print("Building networks...")
-    resnet152_15 = pickle.load(open("pickles/models/entire_nuswide_model_15.p", "rb")).cpu().eval()
-    search_engine.add_model(
-        resnet152_15, "ResNet152-15", ["image","text"], [(2048,), (300,)], 64, 
-        desc = "ResNet152 trained with 15 epochs")
-    search_engine.models["ResNet152-15"].add_preprocessing("image", FeatureExtractor("resnet152").get_embedding)
-
-    resnet152_5 = pickle.load(open("pickles/models/entire_nuswide_model_5.p", "rb")).cpu().eval()
-    search_engine.add_model(
-        resnet152_5, "ResNet152-5", ["image","text"], [(2048,), (300,)], 64, 
-        desc = "ResNet152 trained with 5 epochs")
-    search_engine.models["ResNet152-5"].add_preprocessing("image", FeatureExtractor("resnet152").get_embedding)
-
-
-    resnet18_5 = pickle.load(open("pickles/models/entire_nuswide_model_5-18.p", "rb")).cpu().eval()
-    search_engine.add_model(
-        resnet152_15, "ResNet18-5", ["image","text"], [(512,), (300,)], 64, 
-        desc = "ResNet18 trained with 5 epochs")
-    search_engine.models["ResNet18-5"].add_preprocessing("image", FeatureExtractor("resnet18").get_embedding)
+    # Add models
+    # hard-coded numerical values are specific to each model
+    print("Loading models")
+    resnet152_15 = pickle.load(open("pickles/models/entire_nuswide_model_15.p", "rb"))
+    resnet152_5 = pickle.load(open("pickles/models/entire_nuswide_model_5.p", "rb"))
+    resnet18_5 = pickle.load(open("pickles/models/entire_nuswide_model_5-18.p", "rb"))
     
-
-    # Build Datasets
-    print("Building datasets...")
-    image_directory = 'data/Flickr'
-    image_from_idx = [i[0] for i in ImageFolder(image_directory).samples]
-
+    print("Adding models")
+    search_engine.add_model(
+        name = "ResNet152_15", 
+        modalities = ["image","text"], 
+        embedding_nets = [resnet152_15.modalityOneNet, resnet152_15.modalityTwoNet],
+        input_dimensions= [(2048,), (300,)], 
+        output_dimension = 64, 
+        desc = "ResNet152 model trained with 15 epochs")
+    search_engine.add_model(
+        name = "ResNet152_5", 
+        modalities = ["image","text"], 
+        embedding_nets = [resnet152_5.modalityOneNet, resnet152_5.modalityTwoNet],
+        input_dimensions= [(2048,), (300,)], 
+        output_dimension = 64, 
+        desc = "ResNet152 model trained with 5 epochs")
+    search_engine.add_model(
+        name = "ResNet18_5", 
+        modalities = ["image","text"], 
+        embedding_nets = [resnet18_5.modalityOneNet, resnet18_5.modalityTwoNet],
+        input_dimensions= [(512,), (300,)], 
+        output_dimension = 64, 
+        desc = "ResNet18 model trained with 5 epochs")
+    
+    # Since our datasets will use nus-wide feature vectors as the dataset instead of PIL.Image,
+    # we need to add preprocessing feature extractor to deal with images received as a search input
+    print("Adding preprecessors")
+    search_engine.models["ResNet152_15"].add_preprocessing("image", FeatureExtractor("resnet152").get_embedding)
+    search_engine.models["ResNet152_5"].add_preprocessing("image", FeatureExtractor("resnet152").get_embedding)
+    search_engine.models["ResNet18_5"].add_preprocessing("image", FeatureExtractor("resnet18").get_embedding)
+    
+    
+    # Importing datasets...
+    print("Loading datasets")
+    image_from_idx = [i[0] for i in ImageFolder(IMAGE_DIR).samples] # Gets all filenames (targets)
+    
+    # Feature vectors from ResNet18
     image_data18 = np.array([])
-    directory = "data/nuswide_features/resnet18/"
-    filenames = sorted(["{}/{}".format(directory, filename) for filename in os.listdir(directory) if filename[-3:] == "npy"])
+    directory_18 = "data/nuswide_features/resnet18/"
+    filenames = sorted(["{}/{}".format(directory_18, filename) 
+                        for filename in os.listdir(directory_18) if filename[-3:] == "npy"])
     for filename in filenames:
-        image_data18 = np.append(image_data18, np.load(filename))
-
+        image_data18 = np.append(image_data18, np.load(filename,))
+    image_data18.resize(len(image_data18) // 512, 512) # resizing one long 1D vector into appropriate size
+    image_data18 = torch.from_numpy(image_data18).cuda().float()
+    
+    # Feature vectors from ResNet152
     image_data152 = np.array([])
-    directory = "data/nuswide_features/resnet152/"
-    filenames = sorted(["{}/{}".format(directory, filename) for filename in os.listdir(directory) if filename[-3:] == "npy"])
+    directory_152 = "data/nuswide_features/resnet152/"
+    filenames = sorted(["{}/{}".format(directory_152, filename) 
+                        for filename in os.listdir(directory_152) if filename[-3:] == "npy"])
     for filename in filenames:
-        image_data152 = np.append(image_data152, np.load(filename))
-
+        image_data152 = np.append(image_data152, np.load(filename).astype('float32'))
+    image_data152.resize(len(image_data152) // 2048, 2048)
+    image_data152 = torch.from_numpy(image_data152).cuda().float()
+    
+    # Fasttext
     global FAST_TEXT
     FAST_TEXT = pickle.load(open("pickles/word_embeddings/word_embeddings_tensors.p", "rb"))
     fast_text = FAST_TEXT
@@ -81,25 +106,48 @@ def init_engine(app):
     text_data = [None] * len(fast_text)
     for idx, (key, value) in enumerate(fast_text.items()):
         text_from_idx[idx] = key
-        text_data[idx] = (value, idx)
-
-    # Build DataLoaders
-    print("Building dataloaders...")
-    batch_size = 128
-    image18_dataloader = DataLoader(image_data18, batch_size = batch_size)
-    image152_dataloader = DataLoader(image_data152, batch_size = batch_size)
-    text_dataloader = DataLoader(text_data, batch_size = batch_size)
-    search_engine.add_dataset("fast_text", text_dataloader, text_from_idx, "text", (300,))
-    search_engine.add_dataset("nus-wide_18", image18_dataloader, image_from_idx, "image", (512,))
-    search_engine.add_dataset("nus-wide_152", image152_dataloader, image_from_idx, "image", (2048,))
-
-    #Build Indexes
+        text_data[idx] = value.cuda()
+        
+    # Dataloaders (not really necessary if we've already extracted embeddings)
+    image18_dataloader = DataLoader(image_data18, batch_size = BATCH_SIZE)
+    image152_dataloader = DataLoader(image_data152, batch_size = BATCH_SIZE)
+    text_dataloader = DataLoader(text_data, batch_size = BATCH_SIZE)
+    
+    # Finally adding the datasets to SearchEngine
+    print("Adding datasets")
+    search_engine.add_dataset(
+        name = "nus-wide_18", 
+        data = image18_dataloader, 
+        targets = image_from_idx, 
+        modality = "image", 
+        dimension = (512,))
+    search_engine.add_dataset(
+        name = "nus-wide_152", 
+        data = image152_dataloader, 
+        targets = image_from_idx, 
+        modality = "image", 
+        dimension = (2048,))
+    search_engine.add_dataset(
+        name = "fast_text", 
+        data = text_dataloader, 
+        targets = text_from_idx, 
+        modality = "text", 
+        dimension = (300,))
+    
+    # Build Indexes
     print("Building Indexes")
-    search_engine.build_index("fast_text")
-    search_engine.build_index("nus-wide")
-
-    #Finished
+    search_engine.build_index(dataset_name = "nus-wide_18", model_name = "ResNet18_5", load_embeddings = LOAD_EMBED)
+    search_engine.build_index(dataset_name = "nus-wide_152", model_name = "ResNet152_5", load_embeddings = LOAD_EMBED)
+    search_engine.build_index(dataset_name = "nus-wide_152", model_name = "ResNet152_15", load_embeddings = LOAD_EMBED)
+    search_engine.build_index(dataset_name = "fast_text", model_name = "ResNet18_5", load_embeddings = LOAD_EMBED)
+    search_engine.build_index(dataset_name = "fast_text", model_name = "ResNet152_5", load_embeddings = LOAD_EMBED)
+    search_engine.build_index(dataset_name = "fast_text", model_name = "ResNet152_15", load_embeddings = LOAD_EMBED)
+    
+    # Done
+    print("Finished initializing SearchEngine")
     app.search_engine = search_engine
+    
+    
 
 def target_to_tensor(target, modality):
     search_engine = app.search_engine
